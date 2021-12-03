8237b4c669e5c43c9046e94373d5b3df414a7263
Timothy Sweetser <tsweetser@gmail.com>
2021-12-03 10:12:13 -0500

    oxygen

--- 2021/3/run.py
+++ 2021/3/run.py
@@ -8,10 +8,10 @@ ZERO = "0"
 
 
 
-def most_common(lines, i) -> str:
+def most_common(lines, i: int) -> str:
     chars = [line[i] for line in lines]
     c = Counter(chars)
-    assert set(c.keys()) == {"0", "1"}
+    # assert set(c.keys()) == {"0", "1"}, c.keys()
     if c[ZERO] > c[ONE]:
         return ZERO
     else:
@@ -37,12 +37,23 @@ def get_epsilon(gamma: str) -> str:
     bits = [1 - int(g) for g in gamma]
     return "".join([str(b) for b in bits])
 
-def run(fname, part=1):
+
+def get_oxygen(lines) -> str:
+    oxygen = ""
+    n = len(lines[0])
+    filtered_lines = lines[:]
+    for i in range(n):
+        bit = most_common(filtered_lines, i)
+        oxygen += bit
+        filtered_lines = [line for line in filtered_lines if line[i] == bit]
+    return oxygen
+
+def run(fname, part=2):
     with open(fname, "rb") as f:
-        lines = f.readlines()
-    lines = [s.decode("utf-8").strip() for s in lines]
+        all_lines = f.readlines()
+    all_lines = [s.decode("utf-8").strip() for s in all_lines]
     if part == 1:
-        gamma = get_gamma(lines)
+        gamma = get_gamma(all_lines)
         eps = get_epsilon(gamma)
         print("gamma=%s, epsilon=%s in binary" % (gamma, eps))
         eps = binary_to_int(eps)
@@ -53,6 +64,10 @@ def run(fname, part=1):
             assert answer == 198
         else:
             assert answer == 3912944
+    else:
+        oxygen = get_oxygen(all_lines)
+        assert binary_to_int(oxygen) == 23, oxygen
+
 
 if __name__ == "__main__":
     fire.Fire(run)
