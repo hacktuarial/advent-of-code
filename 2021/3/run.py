18cd2ee869f995d6a2dcdeb263c01ae60a5db622
Timothy Sweetser <tsweetser@gmail.com>
2021-12-03 10:03:12 -0500

    refactoring

--- 2021/3/run.py
+++ 2021/3/run.py
@@ -3,11 +3,20 @@ from collections import Counter
 
 import fire
 
+ONE = "1"
+ZERO = "0"
+
+
 
 def most_common(lines, i) -> str:
     chars = [line[i] for line in lines]
-    c = Counter(chars).most_common(1)
-    return c[0][0]
+    c = Counter(chars)
+    assert set(c.keys()) == {"0", "1"}
+    if c[ZERO] > c[ONE]:
+        return ZERO
+    else:
+        # in case of tie, return 1
+        return ONE
 
 def get_gamma(lines) -> str:
     n = len(lines[0])
@@ -28,16 +37,22 @@ def get_epsilon(gamma: str) -> str:
     bits = [1 - int(g) for g in gamma]
     return "".join([str(b) for b in bits])
 
-def run(fname):
+def run(fname, part=1):
     with open(fname, "rb") as f:
         lines = f.readlines()
     lines = [s.decode("utf-8").strip() for s in lines]
-    gamma = get_gamma(lines)
-    eps = get_epsilon(gamma)
-    print("gamma=%s, epsilon=%s in binary" % (gamma, eps))
-    eps = binary_to_int(eps)
-    gamma = binary_to_int(gamma)
-    print("gamma=%d, epsilon=%d, product=%d" % (gamma, eps, gamma*eps))
+    if part == 1:
+        gamma = get_gamma(lines)
+        eps = get_epsilon(gamma)
+        print("gamma=%s, epsilon=%s in binary" % (gamma, eps))
+        eps = binary_to_int(eps)
+        gamma = binary_to_int(gamma)
+        answer = eps * gamma
+        print("gamma=%d, epsilon=%d, product=%d" % (gamma, eps, answer))
+        if fname == "sample_input.txt":
+            assert answer == 198
+        else:
+            assert answer == 3912944
 
 if __name__ == "__main__":
     fire.Fire(run)
