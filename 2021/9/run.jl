e99fcc823cb59eeea641f58feef50026cbc56b40
Timothy Sweetser <tsweetser@gmail.com>
2021-12-09 12:04:02 -0500

    separate functions

--- 2021/9/run.jl
+++ 2021/9/run.jl
@@ -11,43 +11,49 @@ end
 @assert 1 == 1
 
 function read_input_string(in::String)
-    rows = split(in, "\n")
+    rows = filter(row -> length(row) > 0, split(in, "\n"))
     n_cols = length(rows[1])
     n_rows = length(rows)
-    # mat = Matrix{Int}(n_rows, n_cols)
-    # mat = Array{Int}(n_rows, n_cols)
     mat = zeros(Int, (n_rows, n_cols))
     for (i, row) in enumerate(rows)
         for (j, v) in enumerate(row)
             mat[i, j] =  parse(Int, v)
         end
     end
-    # println(size(mat))
     mat
 end
 
 function findLowPoints(mat::Matrix{Int})
-    riskLevel::Int = 0
+    points = []
     for row=1:size(mat, 1)
         for col=1:size(mat, 2)
             if isLowPoint(mat, row, col)
-                # println((row, col))
-                riskLevel += 1 + mat[row, col]
+                points = push!(points, (row, col))
             end
         end
+    end
+    points
+end
+
+function computeRiskLevel(mat::Matrix{Int}, points)
+    riskLevel = 0
+    for point in points
+        riskLevel += 1 + mat[point[1], point[2]]
     end
     return riskLevel
 end
 
 
-input_string =
-"""
-2199943210
-3987894921
-9856789892
-8767896789
-9899965678"""
 
-@assert findLowPoints(read_input_string(input_string)) == 15
+open("sample_input.txt", "r") do f
+    mat = read_input_string(read(f, String))
+    @assert computeRiskLevel(mat, findLowPoints(mat)) == 15
+end
+    
 
 
+
+open("input.txt", "r") do f
+    mat = read_input_string(read(f, String))
+    println(computeRiskLevel(mat, findLowPoints(mat)))
+end
