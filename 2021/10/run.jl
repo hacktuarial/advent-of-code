c553c12fcf24d6340cd87cc9318c5b0e61933983
Timothy Sweetser <tsweetser@gmail.com>
2021-12-10 09:52:16 -0500

    part1

--- 2021/10/run.jl
+++ 2021/10/run.jl
@@ -26,20 +26,21 @@ function find(arr, elt)::Int
     return -1
 end
 
-function isValid(chunk::String)::Bool
-    open_chars =  ['(', '{', '[', '<']
-    close_chars = [')', '}', ']', '>']
+function isValid(chunk::String)
+    empty_char::Char = 'z'
+    open_chars::Array{Char} =  ['(', '{', '[', '<']
+    close_chars ::Array{Char}= [')', '}', ']', '>']
     stack:: Array{Char} = []
     for c::Char in chunk
         if c in open_chars
             stack = push!(stack, c)
         else
-            # it's a closing character. figure out which one
+            # c is a closing character
             try
                 d = pop!(stack)
                 # close must have matching open
                 if find(open_chars, d) != find(close_chars, c)
-                    return false
+                    return c
                 end
             catch LoadError
                 return false
@@ -47,17 +48,32 @@ function isValid(chunk::String)::Bool
         end
     end
     # if you get to the end, and there's nothing left, it's valid
-    return length(stack) == 0
+    return empty_char
 end
 
-@assert isValid("()")
-@assert isValid("(())")
-@assert ! isValid("(((")
-@assert ! isValid(")")
-@assert isValid("(((((((((())))))))))")
+@assert isValid("()") == 'z'
+@assert isValid("(())") == 'z'
+@assert isValid("(((") == 'z'
+# @assert isValid(")")
+@assert isValid("(((((((((())))))))))") == 'z'
 
-@assert isValid("([])")
-@assert isValid("([]{}<>)")
+@assert isValid("([])") == 'z'
+@assert isValid("([]{}<>)") == 'z'
 
-@assert ! isValid("{([(<{}[<>[]}>{[]{[(<()>")
-@assert ! isValid("[[<[([]))<([[{}[[()]]]")
+@assert isValid("{([(<{}[<>[]}>{[]{[(<()>") == '}'
+@assert isValid("[[<[([]))<([[{}[[()]]]") == ')'
+
+
+open("sample_input.txt", "r") do f
+    lines = readlines(f)
+    corrupted = map(isValid, lines)
+    total = reduce((x, y) -> x+y, map(score, corrupted))
+    @assert total == 26397
+end
+
+open("input.txt", "r") do f
+    lines = readlines(f)
+    corrupted = map(isValid, lines)
+    total = reduce((x, y) -> x+y, map(score, corrupted))
+    println(total)
+end
