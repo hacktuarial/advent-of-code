f4c79fc6da77cd8fbb19e90f1f3c63847751f6e7
Timothy Sweetser <tsweetser@gmail.com>
2021-12-10 09:21:28 -0500

    support for (, {, <, [

--- 2021/10/run.jl
+++ 2021/10/run.jl
@@ -17,18 +17,30 @@ end
 
 @assert score(']') == 57
 
+function find(arr, elt)::Int
+    for (i, a) in enumerate(arr)
+        if a == elt
+            return i
+        end
+    end
+    return -1
+end
+
 function isValid(chunk::String)::Bool
-    open = '('
-    close = ')'
-    stack::Array{Char} = []
+    open_chars = ['(', '{', '[', '<']
+    close_chars = [')', '}', ']', '>']
+    stacks::Array{Array{Char}} = [[], [], [], []]
     for c::Char in chunk
-        if c == open
-            stack = push!(stack, c)
+        if c in open_chars
+            index = find(open_chars, c)
+            stacks[index] = push!(stacks[index], c)
         else
+            # it's a closing character. figure out which one
+            index = find(close_chars, c)
             try
-                d = pop!(stack)
+                d = pop!(stacks[index])
                 # close must have matching open
-                if d != open
+                if d != open_chars[index]
                     return false
                 end
             catch LoadError
@@ -37,10 +49,17 @@ function isValid(chunk::String)::Bool
         end
     end
     # if you get to the end, and there's nothing left, it's valid
-    return length(stack) == 0
+    return all(length(stack) == 0 for stack in stacks)
 end
 
 @assert isValid("()")
 @assert isValid("(())")
 @assert ! isValid("(((")
 @assert ! isValid(")")
+@assert isValid("(((((((((())))))))))")
+
+@assert isValid("([])")
+@assert isValid("([]{}<>)")
+
+@assert ! isValid("{([(<{}[<>[]}>{[]{[(<()>")
+@assert ! isValid("[[<[([]))<([[{}[[()]]]")
