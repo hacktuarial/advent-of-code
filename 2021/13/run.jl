4cf0ef4c8415051efe400cfea8b481c15c571cec
Timothy Sweetser <tsweetser@gmail.com>
2021-12-13 11:57:51 -0500

    more generic foldUp

--- 2021/13/run.jl
+++ 2021/13/run.jl
@@ -7,6 +7,18 @@ function isDot(line::String)::Bool
     match(r"[0-9]+,[0-9]+", line) !== nothing
 end
 
+function nonZero(mat::Array{Int})
+    total = 0
+    for i=1:size(mat, 1)
+        for j=1:size(mat, 2)
+            if mat[i, j] > 0
+                total += 1
+            end
+        end
+    end
+    total
+end
+
 function createDotMatrix(dots)
     dim = maximum(map(maximum, dots)) + 1
     dot_matrix = zeros(Int, (dim, dim))
@@ -29,9 +41,34 @@ function readInput(fname::String)
     end
 end
 
+function foldUp(mat::Array{Int}, y::Int)::Array{Int}
+    y += 1  # julia starts indexing at 1
+    dim = size(mat, 1)
+    out = copy(mat[1:(y-1), :])
+    for i=1:(dim-y)
+        out[y - i, :] += mat[y+i, :]
+    end
+    out
+end
+
+actual = foldUp([ 0 0 0 0; 1 0 1 0; 0 1 0 1; 1 1 1 1], 2)
+@assert  actual== [0 0 0 0; 2 1 2 1] 
+
+
+actual = foldUp([0 0 0 1 0 0 1 0 0 1 0 0 0 0 0; 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 1 0 0 0 0 1 0 1 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 1 0 0 0 0 1 0 1 1 0 0 0 0 0; 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0; 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0], 7) 
+expected = [1 0 1 1 0 0 1 0 0 1 0 0 0 0 0; 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0; 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 1 0 1 0 0 1 0 2 1 1 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+@assert actual == expected
+@assert nonZero(expected) === 17
+
+
+@assert foldLeft([1 0 1 1 0 0 1 0 0 1 0 0 0 0 0; 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0; 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 1 0 1 0 0 1 0 2 1 1 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 1 0 1 0 0 1 0 2 1 1 0 0 0 0; 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0; 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0],
+5) == [1 1 1 1 1; 1 0 0 0 1; 1 0 0 0 1; 1 0 0 0 1; 1 1 1 1 1; 0 0 0 0 0; 0 0 0 0 0]
+
 function part1(fname::String)
-    dot_matrix = createDotMatrix(readInput(fname))
-    println(dot_matrix)
+    paper = createDotMatrix(readInput(fname))
+    paper = foldUp(paper, 7)
+    # paper = foldLeft(paper, 5)
+    # println(paper)
 end
 
 part1("sample.txt")
