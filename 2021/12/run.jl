13ab903001a71a5d78b59c582ef5dfdfe6bb4675
Timothy Sweetser <tsweetser@gmail.com>
2021-12-12 22:24:04 -0500

    still not working

--- 2021/12/run.jl
+++ 2021/12/run.jl
@@ -1,12 +1,3 @@
-function findVertices(edges)
-    vertices = Set()
-    for (v1, v2) in edges
-        vertices += v1
-        vertices += v2
-    end
-    vertices
-end
-
 function the_other_one(v, (a, b))
     if v == a
         return b
@@ -14,6 +5,9 @@ function the_other_one(v, (a, b))
         return a
     end
 end
+
+@assert the_other_one("c", ["A", "c"]) == "A"
+@assert the_other_one("A", ["A", "c"]) == "c"
 
 function isSmallCave(vertex)::Bool
     match(r"^[A-Z]", String(vertex)) === nothing
@@ -26,8 +20,8 @@ isBigCave = x -> !isSmallCave(x)
 
 
 
-function findNeighbors(vertex, path, edges)
-    neighbors = []
+function findNeighbors(vertex::String, path::Array{String}, edges)
+    neighbors::Array{String} = []
     for edge in edges
         if vertex in edge
             neighbor = the_other_one(vertex, edge)
@@ -42,20 +36,26 @@ function findNeighbors(vertex, path, edges)
     neighbors
 end
 
+
+function testFindNeighbors()
+    path::Array{String} = []
+    edges::Array{Array{String}} = [["A", "c"]]
+    @assert findNeighbors("c", path, edges) == ["A", ]
+end
+
 function findPath(here::String, destination::String, path::Array{String}, edges)
     # depth-first search
     path = push!(path, here)
-    if here === destination
+    if here == destination
         open("paths.txt", "a") do f
             map(s -> write(f, s * "-"), path)
             write(f, "\n")
         end
+        return
     end
     for vertex in findNeighbors(here, path, edges)
         findPath(vertex, destination, path, edges)
     end
-    # backtrack
-    # pop!(path)
 end
 
 
@@ -63,12 +63,23 @@ end
 
 function part1(fname::String)
     open(fname, "r") do f
-        edges = map(x -> split(x, "-"), readlines(f))
-        edges = map(x -> map(String, x), edges)
-        path::Array{String} = []
-        findPath("start", "end", path, edges)
+        edges = map(x -> map(String, split(x, "-")), readlines(f))
+        empty_path::Array{String} = []
+        # map(println, edges)
+        if fname == "sample1.txt"
+            @assert findNeighbors("start", empty_path, edges) == ["A", "b"]
+            @assert findNeighbors("c", empty_path, edges) == ["A", ]
+            @assert findNeighbors("end", empty_path, edges) == ["A", "b"]
+            @assert findNeighbors("d", empty_path, edges) == ["b", ]
+
+            path = ["start", "b"]
+            # @assert findNeighbors("d", path, edges) == ["d","A", "end" ]
+            println(findNeighbors("end", path, edges))
+
+        end
+        findPath("start", "end", empty_path, edges)
     end
 end
 
 
-part1("sample2.txt")
+part1("sample1.txt")
