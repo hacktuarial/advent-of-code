d22e385cc3140773c4f35951fcd37e26356789f3
Timothy Sweetser <tsweetser@gmail.com>
2021-12-13 12:45:35 -0500

    PIpe not working

--- 2021/13/run.jl
+++ 2021/13/run.jl
@@ -1,3 +1,5 @@
+using Pipe
+
 function parseNumbers(xy::String, delimiter::Char=',')
     (x, y) = split(xy, delimiter)
     [parse(Int, x), parse(Int, y)]
@@ -20,8 +22,8 @@ function nonZero(mat::Array{Int})
 end
 
 function createDotMatrix(dots)
-    n_cols = maximum(map(xy -> x[1], dots))
-    n_rows = maximum(map(xy -> x[2], dots))
+    n_cols = maximum(map(x -> x[1], dots)) + 1
+    n_rows = maximum(map(x -> x[2], dots)) + 1
     dot_matrix = zeros(Int, (n_rows, n_cols))
     for (col, row) in dots
         # julia starts indexing at 1 :)
@@ -46,18 +48,24 @@ function foldUp(mat::Array{Int}, y::Int)::Array{Int}
     y += 1  # julia starts indexing at 1
     dim = size(mat, 1)
     out = copy(mat[1:(y-1), :])
-    for i=1:(dim-y)
-        out[y - i, :] += mat[y+i, :]
+    rows_to_copy = dim - y
+    for i = 1:rows_to_copy
+        from = y + i
+        to = y - i
+        out[to, :] += mat[from, :]
     end
     out
 end
 
 function foldLeft(mat::Array{Int}, x::Int)::Array{Int}
-    x += 1
+    x += 1  # julia starts indexing at 1
     dim = size(mat, 2)
     out = copy(mat[:, 1:(x-1)])
-    for j=1:(dim-x)
-        out[:, x - j] += mat[:, x + j]
+    columns_to_copy = dim - x
+    for i = 1:columns_to_copy
+        from = x + i
+        to = x - i
+        out[:, to] += mat[:, from]
     end
     out
 end
@@ -81,16 +89,29 @@ expected = [1 0 1 1 0 0 1 0 0 1 0 0 0 0 0; 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0; 0 0 0 
 @assert nonZero(expected) === 17
 
 
-actual = copy(expected)
-expected = [1 1 1 1 1; 1 0 0 0 1; 1 0 0 0 1; 1 0 0 0 1; 1 1 1 1 1; 0 0 0 0 0; 0 0 0 0 0]
-println(actual)
-@assert foldLeft(actual, 5) == expected
-
 function part1(fname::String)
     paper = createDotMatrix(readInput(fname))
+    @assert size(paper) === (15, 11)
     paper = foldUp(paper, 7)
+    @assert size(paper) === (7, 11)
     paper = foldLeft(paper, 5)
-    # println(paper)
+    @assert size(paper) === (7, 5)
+    return nonZero(paper)
 end
 
 part1("sample.txt")
+"input.txt" |> readInput |> createDotMatrix |>
+    foldLeft(_, 655) |>
+    foldUp(_, 447) |>
+    foldUp(_, 327) |>
+    foldLeft(_, 223) |>
+    foldUp(_, 163) |>
+    foldLeft(_, 111) |>
+    foldUp(_, 81) |>
+    foldLeft(_, 55) |>
+    foldUp(_, 40) |>
+    foldLeft(_, 27) |>
+    foldLeft(_, 13) |>
+    foldLeft(_, 6) |>
+    println
+
