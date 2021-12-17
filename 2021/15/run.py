cd53005e57609f1135cdab9f6b7443a6199ad962
Timothy Sweetser <tsweetser@gmail.com>
2021-12-17 09:36:12 -0500

    working for sample

--- 2021/15/run.py
+++ 2021/15/run.py
@@ -18,7 +18,8 @@ def read_input(fname):
     return np.array(matrix)
 
 
-def get_neighbors(matrix, row, col):
+def get_neighbors(matrix, vertex):
+    row, col = vertex
     neighbors = []
     if row - 1 >= 0:
         neighbors.append((row - 1, col))  # up
@@ -31,36 +32,42 @@ def get_neighbors(matrix, row, col):
     return neighbors
 
 
-assert set(get_neighbors(np.ones((2, 2)), 0, 0)) == set([(0, 1), (1, 0)])
+assert set(get_neighbors(np.ones((2, 2)), (0, 0))) == set([(0, 1), (1, 0)])
+assert set(get_neighbors(np.ones((3, 3)), (0, 1))) == set([(0, 0), (0, 2), (1, 1)])
+assert set(get_neighbors(np.ones((3, 3)), (1, 1))) == set(
+    [(0, 1), (1, 0), (1, 2), (2, 1)]
+)
 
 
-def find_min_distance(matrix):
+def find_min_distance(matrix, known) -> Tuple[int]:
     """Returns the index of the minimum value"""
-    min = (0, 0)
+    min_value = np.inf
+    which_min = None
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
-            if matrix[i, j] < matrix[min[0], min[1]]:
-                min = (i, j)
-    return min
+            if (i, j) in known:
+                continue
+            if matrix[i, j] < min_value:
+                which_min = (i, j)
+                min_value = matrix[i, j]
+    return which_min
 
 
 def dijkstra(matrix, start: Tuple, end: Tuple):
     known = {start}
     distances = np.ones(matrix.shape) * np.inf
-    for neighbor in get_neighbors(matrix, *start):
-        logger.info(neighbor)
+    for neighbor in get_neighbors(matrix, start):
         # no cost incurred at starting vertex
-        distances[neighbor[0], neighbor[1]] = distances[neighbor[0], neighbor[1]]
+        distances[neighbor[0], neighbor[1]] = matrix[neighbor[0], neighbor[1]]
     last = start
     while last != end:
-        v = find_min_distance(distances)
-        for neighbor in get_neighbors(matrix, *v):
+        v = find_min_distance(distances, known)
+        for neighbor in get_neighbors(matrix, v):
             current_distance = distances[neighbor[0], neighbor[1]]
             new_distance = distances[v[0], v[1]] + matrix[neighbor[0], neighbor[1]]
             distances[neighbor[0], neighbor[1]] = min(current_distance, new_distance)
         last = v
-        known.update(v)
-        logger.info("I've visited %d vertices", len(known))
+        known = known.union({v})
     return distances[end[0], end[1]]
 
 
@@ -68,8 +75,10 @@ def part1(fname):
     matrix = read_input(fname)
     logger.info(matrix.shape)
     # assuming it's square
-    cost = dijkstra(matrix, (0, 0), (len(matrix), len(matrix)))
+    start = (0, 0)
+    end = (matrix.shape[0] - 1, matrix.shape[1] - 1)
+    cost = dijkstra(matrix, start, end)
     return cost
 
 
-part1("sample.txt")
+assert 40 == part1("sample.txt")
