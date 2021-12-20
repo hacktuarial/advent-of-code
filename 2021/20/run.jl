a4cc8bf9c3b15c684c01a11f4b60a946befc5b94
Timothy Sweetser <tsweetser@gmail.com>
2021-12-20 10:54:52 -0500

    6561 is too high

--- 2021/20/run.jl
+++ 2021/20/run.jl
@@ -53,8 +53,7 @@ end
 
 
 
-function readImage(s::String)::Array{Int}
-    lines = split(s, "\n")
+function readImage(lines)::Array{Int}
     out = zeros((length(lines), length(lines[1])))
     for (row, line) in enumerate(lines)
         for (col, c) in enumerate(line)
@@ -78,31 +77,31 @@ end
             
 
 
-algorithm="00101001111101010101110110000011101101001110111100111110010000100100110011100111111011100011110010011111001100101111100011010100101100101000000101110111111011101111000101101100100100111110000010100001110010110000001000001001001001100100011011111101111011110101000100000001001010100011110110100000010010001101011001000110101100111010000001010000000101010111101110110001000001111010010010110100001100101111000011000110010001000000101000000010000000110011110010001010100011001010011100111110000000010011110000001001"
-img = """#..#.
-#....
-##..#
-..#..
-..###"""
-println(prettyPrint(readImage(img)))
-# println(prettyPrint(pad(readImage(img), 1)))
-# println(prettyPrint(pad(readImage(img), 2)))
-output = doStep(algorithm, readImage(img))
-output = doStep(algorithm, output)
-@assert sum(output) === 35
-
 
 function readAlgorithm(algo::String)::String
-    replace(replace(algo, '#' => '1'), '.' => '0')
+    out = replace(replace(algo, '#' => '1'), '.' => '0')
+    @assert length(algo) === length(out)
+    out
 end
 
 @assert readAlgorithm("###..#..") === "11100100"
 
+
+open("sample.txt", "r") do f
+    lines = readlines(f)
+    algorithm = readAlgorithm(popfirst!(lines))
+    mat = readImage(lines[2:end])
+    println(prettyPrint(mat))
+    for _ in 1:2
+        mat = doStep(algorithm, mat)
+    end
+    @assert sum(mat) === 35
+end
+
 open("input.txt", "r") do f
     lines = readlines(f)
     algorithm = readAlgorithm(popfirst!(lines))
-    mat = join(lines[3:end], "\n")
-    mat = readImage(mat)
+    mat = readImage(lines[2:end])
     for _ in 1:2
         mat = doStep(algorithm, mat)
     end
