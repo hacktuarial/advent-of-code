89b53bb6db4e35f20ee770fe9d252afc1983096a
Timothy Sweetser <tsweetser@gmail.com>
2021-12-20 17:12:49 -0500

    create snailfish from string

---
+++ 2021/18/snailfish.py
@@ -0,0 +1,43 @@
+class Snailfish:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def reduce(self):
+        pass
+
+    def __eq__(self, other):
+        if not isinstance(other, Snailfish):
+            return False
+        return self.left == other.left and self.right == other.right
+
+    def __str__(self):
+        return "[" + str(self.left) + "," + str(self.right) + "]"
+
+
+# print(Snailfish(1, 2))
+# print(Snailfish(Snailfish(1, 2), 3))
+# print(Snailfish(9, Snailfish(8, 7)))
+
+
+def read(string: str) -> Snailfish:
+    numbers = []
+    for s in string:
+        if s == "[" or s == ",":
+            continue
+        elif s == "]":
+            right = numbers.pop()
+            left = numbers.pop()
+            numbers.append(Snailfish(left, right))
+        else:
+            numbers.append(int(s))
+    return numbers[0]
+
+
+assert read("[1,2]") == Snailfish(1, 2)
+
+fish = "[[1,9],[8,5]]"
+assert str(read(fish)) == fish, str(read(fish))
+fish = "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]"
+assert str(read(fish)) == fish
+read("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]")
