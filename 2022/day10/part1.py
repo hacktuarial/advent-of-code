aeaebecf4ba2499a7dbf59005bc4d7cef0bb36ea
Timothy Sweetser <tsweetser@gmail.com>
2022-12-16 13:35:31 -0500

    so much easier with a class

--- 2022/day10/part1.py
+++ 2022/day10/part1.py
@@ -1,30 +1,43 @@
-
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
+
+class State:
+    def __init__(self, t=0, X=1):
+        self.t = t
+        self.X = X
+        self.buffer = 0
+        self.signal_strength = 0
+
+    def cycle(self, instruction):
+        self.t += 1
+        # this happens mid-cycle
+        if (self.t - 20) % 40 == 0:
+            self.signal_strength += self.t * self.X
+        # end of cycle
+        self.X += self.buffer
+        self.buffer = 0
+        if instruction.startswith("add"):
+            value = int(instruction.split()[-1])
+            assert self.buffer == 0
+            self.buffer = value
+            self.cycle(instruction="noop")
+
+
+state = State()
 with open("sample.txt", "r") as f:
     instructions = f.readlines()
-
-t = 0
-X = 1
-signal_strength = 0
 for instruction in instructions:
-    if instruction.startswith("noop"):
-        t += 1
-        logging.debug(t, X)
-        continue
-    operation, value = instruction.split()
-    assert operation == "addx"
-    value = int(value)
-    for i in range(2):
-        t += 1
-        if (t - 20) % 40 == 0:
-            logging.info(f"Signal strength at time {t} is {t*X}")
-            signal_strength += t * X
-    X += value
-    logging.debug(t, X)
-            
-                    
-                
+    state.cycle(instruction)
 
-assert signal_strength == 13140, signal_strength
+assert state.signal_strength == 13140, signal_strength
+
+
+part1 = State()
+with open("input.txt", "r") as f:
+    instructions = f.readlines()
+for instruction in instructions:
+    part1.cycle(instruction)
+
+print(part1.signal_strength)
