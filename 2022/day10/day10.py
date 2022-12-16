c913ace772ede11d7dee322a4345ae46bc6d231d
Timothy Sweetser <tsweetser@gmail.com>
2022-12-16 13:52:16 -0500

    part2

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -9,12 +9,21 @@ class State:
         self.X = X
         self.buffer = 0
         self.signal_strength = 0
+        self.part2 = ["0"] * 241
+
+    def draw(self):
+        t = self.t % 40 - 1
+        if self.X - 1 <= t <= self.X + 1:
+            self.part2[self.t] = "#"
+        else:
+            self.part2[self.t] = "."
 
     def cycle(self, instruction):
         self.t += 1
         # this happens mid-cycle
         if (self.t - 20) % 40 == 0:
             self.signal_strength += self.t * self.X
+        self.draw()
         # end of cycle
         self.X += self.buffer
         self.buffer = 0
@@ -32,7 +41,8 @@ for instruction in instructions:
     state.cycle(instruction)
 
 assert state.signal_strength == 13140, signal_strength
-
+for i in range(0, 240, 40):
+    print("".join(state.part2[i : i + 40]))
 
 part1 = State()
 with open("input.txt", "r") as f:
@@ -40,4 +50,6 @@ with open("input.txt", "r") as f:
 for instruction in instructions:
     part1.cycle(instruction)
 
-print(part1.signal_strength)
+print(f"signal strength={part1.signal_strength}")
+for i in range(0, 240, 40):
+    print("".join(part1.part2[i : i + 40]))
