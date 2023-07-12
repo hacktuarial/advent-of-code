19e5a65bb765f08a3d0404a3cef014aea4863e10
Timothy Sweetser <tim.sweetser@warnermedia.com>
2023-07-12 10:12:28 -0400

    part1

--- 2022/day7/day7.go
+++ 2022/day7/day7.go
@@ -16,6 +16,17 @@ type Directory struct {
 	files    int
 }
 
+func (dir Directory) sizes() []int {
+	sizes := make([]int, 1)
+	sizes[0] = dir.size()
+	for _, child := range dir.children {
+		for _, size := range child.sizes() {
+			sizes = append(sizes, size)
+		}
+	}
+	return sizes
+}
+
 func (dir Directory) size() int {
 	size := dir.files
 	for _, child := range dir.children {
@@ -24,11 +35,11 @@ func (dir Directory) size() int {
 	return size
 }
 
-func (dir Directory) addChild(child *Directory) {
+func (dir *Directory) addChild(child *Directory) {
 	dir.children = append(dir.children, child)
 }
 
-func (dir Directory) addFile(filesize int) {
+func (dir *Directory) addFile(filesize int) {
 	dir.files += filesize
 }
 
@@ -41,10 +52,11 @@ func isDirectory(line string) bool {
 }
 
 func isCD(line string) bool {
-	return strings.Split(line, " ")[1] == "cd"
+	return line[:4] == "$ cd"
 }
+
 func isLS(line string) bool {
-	return strings.Split(line, " ")[1] == "ls"
+	return line[:4] == "$ ls"
 }
 
 func day7(filename string) int {
@@ -64,62 +76,79 @@ func day7(filename string) int {
 	}
 	// fmt.Println(strings.Join(lines, "\n")) . // same as input
 	// skip the first one, which just does cd into /
-	var i = 1
+	var i int
 	for i = 1; i < len(lines); i++ {
-		// fmt.Println("current line is `" + lines[i] + "`")
-		if isCommand(lines[i]) {
-			if isCD(lines[i]) {
-				destination := strings.Split(lines[i], " ")[2]
-				if destination == ".." {
-					fmt.Println(currentDir.name)
-					currentDir = currentDir.parent
-				} else {
-					for _, child := range currentDir.children {
-						if child.name == destination {
-							currentDir = child
-							break
-						}
-					}
-					panic("could not find subdirectory " + destination + " of parent " + currentDir.name)
-				}
-
-			} else if isLS(lines[i]) {
-				// Need to look at the next lines
-				for i = i + 1; !isCommand(lines[i]); i++ {
-					if isDirectory(lines[i]) {
-						child := Directory{name: strings.Split(lines[i], " ")[1],
-							parent: currentDir, children: make([]*Directory, 0),
-							files: 0}
-						currentDir.addChild(&child)
-						fmt.Println("I added directory " + child.name + " to " + currentDir.name)
-					} else {
-						// it's a file
-						oldSize := int64(currentDir.files)
-						fileName := strings.Split(lines[i], " ")[1]
-						fileSize, _ := strconv.Atoi(strings.Split(lines[i], " ")[0])
-						currentDir.addFile(fileSize)
-						newSize := int64(currentDir.files)
-						fmt.Println("I added file " + fileName + " to " + currentDir.name)
-						fmt.Println("its size increased from " + strconv.FormatInt(oldSize, 10) +
-							" to " + strconv.FormatInt(newSize, 10))
-					}
-				}
-
+		fmt.Println("current working directory is `" + currentDir.name + "`")
+		if isCD(lines[i]) {
+			destination := strings.Split(lines[i], " ")[2]
+			fmt.Println("I want to change working directory to " + destination)
+			if destination == ".." {
+				currentDir = currentDir.parent
 			} else {
-				panic("Unknown command" + lines[i])
+				for _, child := range currentDir.children {
+					if child.name == destination {
+						currentDir = child
+						fmt.Println("I changed working directory to " + currentDir.name)
+						break
+					} else {
+						fmt.Println(child.name + " != " + destination)
+					}
+				}
+				// msg := "could not find subdirectory " + destination + " of parent " + currentDir.name + ". Its children are ("
+				// for _, child := range currentDir.children {
+				// 	msg += child.name + ","
+				// }
+				// msg += ")"
+				// panic(msg)
+
 			}
 
+		} else if isLS(lines[i]) {
+			// Need to look at the next lines
+			for i = i + 1; i < len(lines) && !isCommand(lines[i]); i++ {
+				if isDirectory(lines[i]) {
+					child := Directory{name: strings.Split(lines[i], " ")[1],
+						parent: currentDir, children: make([]*Directory, 0),
+						files: 0}
+					currentDir.addChild(&child)
+					fmt.Println("I added directory " + child.name + " to " + currentDir.name)
+				} else {
+					// it's a file
+					oldSize := int64(currentDir.files)
+					fileName := strings.Split(lines[i], " ")[1]
+					fileSize, _ := strconv.Atoi(strings.Split(lines[i], " ")[0])
+					currentDir.addFile(fileSize)
+					newSize := int64(currentDir.files)
+					fmt.Println("I added file " + fileName + " to " + currentDir.name)
+					fmt.Println("its size increased from " + strconv.FormatInt(oldSize, 10) +
+						" to " + strconv.FormatInt(newSize, 10))
+				}
+			}
+			// fmt.Println("I broke out of the ls loop. i=" + strconv.FormatInt(int64(i), 10))
+			i--
+
 		} else {
-			panic("don't know what to do with this: " + lines[i])
+			panic("Unknown command" + lines[i])
+		}
+
+	}
+	sizes := root.sizes()
+	total := 0
+	for _, size := range sizes {
+		if size < 100000 {
+			total += size
 		}
 	}
-	return 0
+	return total
 }
 
 func main() {
 	// part 1
-	// fmt.Println("the answer to part1 is ")
-	fmt.Println(day7("sample.txt"))
+	if day7("sample.txt") != 95437 {
+		panic("wrong answer to sample")
+	}
+	fmt.Println("the answer to part1 is ")
+	fmt.Println(day7("input.txt"))
 	// part 2
 	// fmt.Println("the answer to part2 is ")
 	// fmt.Println(day6("input.txt", 14))
